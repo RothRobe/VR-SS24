9924782a630406a2b95e238457fa08613b32a3a1
jessis99 <jessi@schifferweb.de>
2024-07-10 12:03:36 +0200

    Überprüfung Baumeister

--- Projekt 04 - Serious Game/Assets/Skripts/Baumeister.cs
+++ Projekt 04 - Serious Game/Assets/Skripts/Baumeister.cs
@@ -10,11 +10,81 @@ public class Baumeister : MonoBehaviour
     public TextMeshProUGUI textMeshPro;
     public Button nextButton;
     public GameObject baumeister;
-    private bool isRight = false;
+
+    public GameObject spielfeld;
+    public GameObject[] spielsteine;
+    public Vector3Int boardSize = new Vector3Int(5, 5, 2); 
+
+    public List<int[,,]> musterListe = new List<int[,,]>{
+        new int[,,] {
+            {
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 1, 1 }
+            },
+            {
+                { 1, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0 },
+                { 1, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0 },
+                { 1, 0, 0, 0, 1 }
+            }
+        },
+        new int[,,] {
+            {
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 0, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 }
+            },
+            {
+                { 1, 0, 1, 0, 1 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 1, 0, 1, 0, 1 }
+            }
+        },
+        new int[,,] {
+            {
+                { 1, 1, 0, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 }
+            },
+            {
+                { 1, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0 },
+                { 1, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0 },
+                { 1, 0, 0, 0, 1 }
+            }
+        },
+        new int[,,] {
+            {
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 0 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 }
+            },
+            {
+                { 1, 0, 1, 0, 1 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 1, 0, 1, 0, 1 }
+            }
+        }
+    };
+
 
     public void OnButtonClick(Button clickedButton){
         if(clickedButton.name == "Button accept"){
-            prove();
             buttonAccept();
         }
         if(clickedButton.name == "Button Next"){
@@ -23,7 +93,7 @@ public class Baumeister : MonoBehaviour
     }
 
     public void buttonAccept(){
-        if(isRight){
+        if(proveAllPatterns()){
             textMeshPro.text += "\nHerzlichen Glückwunsch! Das sieht gut aus! Du erhälst einen Punkt.";
             //Gesamtpunktzahl erhöhen
         }else{
@@ -36,8 +106,53 @@ public class Baumeister : MonoBehaviour
         nextButton.gameObject.SetActive(true);
     }
 
-    void prove(){
-        //noch prüfen, ob die Lösung richtig ist
-        isRight = true;
+    bool provePattern(int[,,] muster){
+        int[,,] field = new int[boardSize.x, boardSize.y, boardSize.z];
+        
+        Vector3 spielfeldPosition = spielfeld.transform.position;
+        Vector3 spielfeldScale = spielfeld.transform.localScale;
+
+        float cellBreite = spielfeldScale.x / boardSize.x;
+        float cellHöhe = spielfeldScale.y / boardSize.y;
+        float cellTiefe = spielfeldScale.z / boardSize.z;
+
+        foreach (GameObject stein in spielsteine)
+        {
+            Vector3 localPos = stein.transform.position - spielfeldPosition;
+            int x = Mathf.FloorToInt(localPos.x / cellBreite);
+            int y = Mathf.FloorToInt(localPos.y / cellHöhe);
+            int z = Mathf.FloorToInt(localPos.z / cellTiefe);
+
+            
+            if (x >= 0 && x < boardSize.x && y >= 0 && y < boardSize.y && z >= 0 && z < boardSize.z)
+            {
+                field[x, y, z] = 1;
+            }
+        }
+
+        for (int x = 0; x < boardSize.x; x++)
+        {
+            for (int y = 0; y < boardSize.y; y++)
+            {
+                for (int z = 0; z < boardSize.z; z++)
+                {
+                    if (field[x, y, z] != muster[x, y, z])
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool proveAllPatterns() {
+        foreach (var muster in musterListe){
+            if (provePattern(muster)){
+                return true;
+            }
+        }
+        return false;
     }
 }
